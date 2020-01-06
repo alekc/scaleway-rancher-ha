be740477c3261bf4450bb79e6644d662bd7ea333
Alexander Chernov <alekcander@gmail.com>
2020-01-06 16:07:08 +0000

    Brought back apt

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ resource "scaleway_instance_server" "rancherserver" {
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 
-#apt-get update
-#apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
 #workaround to avoid "Failed to get job complete status for job rke-network-plugin-deploy-job in namespace kube-system"
 docker pull rancher/pause:3.1
